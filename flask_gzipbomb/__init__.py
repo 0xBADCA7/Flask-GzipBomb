3257b00ec07773b1cce881a4b2d5c80a44fdd953
Piotr Kuszaj <peterkuszaj@gmail.com>
2017-07-10 12:39:36 +0200

    setup.py and package description

--- flask_gzipbomb/__init__.py
+++ flask_gzipbomb/__init__.py
@@ -1,7 +1,58 @@
 """
+Flask-GzipBomb
+--------------
+
+Gzip Bomb responses for Flask.
+
+This package provides an extension to Response class, **GzipBombResponse**,
+which can be used as a defensive measure for various vuln scanners, dictionary
+attacks etc. It creates a response containing a gzipped stream of zeroes with
+varying number of rounds (to achieve minimal size of response's content).
+
+Example:
+
+    >>> from flask import Flask
+    >>> from flask_gzipbomb import GzipBombResponse
+    >>>
+    >>> app = Flask(__name__)
+    >>>
+    >>> @app.route('/tiny-bomb')
+    ... def gzipped():
+    ...     return GzipBombResponse(size='1M')
+    >>>
+    >>> app.run()
+
+    >>> import gzip
+    >>> import requests
+    >>>
+    >>> r = requests.get('http://localhost:5000/tiny-bomb')
+    >>> r.headers['content-encoding']
+    'gzip,gzip'
+    >>> len(r.content) # gzipped content length in bytes
+    64
+    >>> data = gzip.decompress(r.content)
+    >>> data = gzip.decompress(data)
+    >>> len(data) # decompressed content length in bytes
+    1048576
+
+GzipBombResponse accepts all arguments accepted by Response class with
+additional *size* parameter, describing response content length in bytes
+after decompression. Possible values:
+
+    '1k', '10k', '100k', '1M', '10M', '100M', '1G', '10G'
+
+with k, M and G denoting kilobyte, megabyte and gigabyte. Any other value
+will result raise a KeyError. By default *size* is set to '10M', however
+it is recommended to use '10G' for achieving desired effect.
+
+**This package is for protection and educational purposes only. Using it
+for any malicious purpose is strictly prohibited.**
 """
 from flask import Response
 from ._rawdata import rawdata
+
+
+__version__ = '0.1.0'
 
 
 class GzipBombResponse(Response):
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from flask_gzipbomb import (
+    __version__ as version,
+    __doc__ as long_description
+)
+
+setup(
+    name='Flask-GzipBomb',
+    version=version,
+    license='MIT',
+    author='Piotr Kuszaj',
+    author_email='peterkuszaj@gmail.com',
+    description='Gzip Bomb responses for Flask',
+    long_description=long_description,
+    packages=['flask_gzipbomb'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+    ],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
